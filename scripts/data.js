27ac3885dc786141ea81e8898e2f4d26c7b70b01
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2020-05-03 15:28:32 +0300

    rescructure

---
+++ scripts/data.js
@@ -0,0 +1,15 @@
+let walls;
+let tank;
+let move;
+let spacebar;
+let enter;
+let moveSpeed = 100;
+let playerMoveSpeed = 200;
+let fireSpeed = 300;
+let gameOver = false;
+let hpValue = 100;
+let hpText;
+
+let simpleEnemy = {
+    damage = 20
+}
---
+++ scripts/fire.js
@@ -0,0 +1,29 @@
+function fire(shooter, that, isEnemy = false) {
+    let fireball = that.physics.add.image(shooter.x, shooter.y, "fireball");
+    fireball.angle = shooter.angle;
+    let fireDir = fireball.angle;
+    if (fireDir == 0) fireball.setVelocityY(0 - fireSpeed);
+    if (fireDir == 90) fireball.setVelocityX(fireSpeed);
+    if (fireDir == -180) fireball.setVelocityY(fireSpeed);
+    if (fireDir == -90) fireball.setVelocityX(0 - fireSpeed);
+
+    that.physics.add.collider(fireball, walls, hitWall, null, that);
+    function hitWall(fireball, wall) {
+        fireball.destroy();
+    }
+
+    if (!isEnemy) {
+        that.physics.add.collider(fireball, enemies, hitEnemy, null, that);
+        function hitEnemy(fireball, enemy) {
+            fireball.destroy();
+            enemy.destroy();
+        }
+    } else {
+        that.physics.add.collider(fireball, tank, () => {
+            let damage = simpleEnemy.damage + (Math.random() * 5);
+            if (Math.random() <= 0.5) damage *= -1;
+            hpValue -= damage;
+            fireball.destroy();
+        }, null, that);
+    }
+}
