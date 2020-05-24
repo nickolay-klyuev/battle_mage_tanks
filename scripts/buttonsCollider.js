e384794c5abba8ea072a551f31af9b84774b46ee
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2020-05-24 17:25:59 +0300

    add new scene

--- scripts/buttonsCollider.js
+++ scripts/buttonsCollider.js
@@ -2,7 +2,7 @@ function buttonsCollider(tank, btn, btnString, blockString, that) {
     let btnActive = that.physics.add.sprite(btn.x, btn.y, btnString);
     btnActive.depth = -1;
     btn.destroy();
-    walls.getChildren().forEach((wall) => {
+    that.walls.getChildren().forEach((wall) => {
         if (wall.texture.key.includes(blockString)) wall.destroy();
     });
 }
--- scripts/fire.js
+++ scripts/fire.js
@@ -1,4 +1,10 @@
 function fire(shooter, that, isEnemy = false) {
+    let playerDamage = 30;
+    let fireSpeed = 300;
+    let simpleEnemy = {
+        damage: 20
+    }
+
     let fireball = that.physics.add.image(shooter.x, shooter.y, "fireball");
     fireball.angle = shooter.angle;
     let fireDir = fireball.angle;
@@ -7,7 +13,7 @@ function fire(shooter, that, isEnemy = false) {
     if (fireDir == -180) fireball.setVelocityY(fireSpeed);
     if (fireDir == -90) fireball.setVelocityX(0 - fireSpeed);
 
-    that.physics.add.collider(fireball, walls, hitWall, null, that);
+    that.physics.add.collider(fireball, that.walls, hitWall, null, that);
     function hitWall(fireball, wall) {
         fireball.destroy();
     }
@@ -15,13 +21,13 @@ function fire(shooter, that, isEnemy = false) {
     let randomDamage = Math.round(Math.random() * 5);
     if (Math.random() <= 0.5) randomDamage *= -1;
     if (!isEnemy) {
-        that.physics.add.collider(fireball, enemies, (fireball, enemy) => {
+        that.physics.add.collider(fireball, that.enemies, (fireball, enemy) => {
             enemy.hpValue -= playerDamage + randomDamage;
             fireball.destroy();
         }, null, that);
     } else {
-        that.physics.add.collider(fireball, tank, () => {
-            hpValue -= simpleEnemy.damage + randomDamage;
+        that.physics.add.collider(fireball, that.tank, () => {
+            that.hpValue -= simpleEnemy.damage + randomDamage;
             fireball.destroy();
         }, null, that);
     }
